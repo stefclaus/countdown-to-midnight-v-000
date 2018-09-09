0045e5964b2358b76394279bfe32e89b87bf7d37
Stef Claus <stef.claus@gmail.com>
2018-09-09 16:18:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,14 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1 
   end 
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end 
+  return "HAPPY NEW YEAR!"
 end
