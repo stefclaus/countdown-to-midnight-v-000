4345e2473c70abb3d638f7fe192b6ee5778ac5fe
Stef Claus <stef.claus@gmail.com>
2018-09-09 15:58:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(countdown)
+  while countdown > 0 
+    puts "#{countdown} SECOND(S)!"
+  countdown -= 1 
+  end 
+  return "HAPPY NEW YEAR!"
 end
